1df9a37d2faced531533643d0c730c94f28cb4ef
Ran Kong <rkbriankong@gmail.com>
2022-02-07 16:01:32 -0600

    012

---
+++ 013.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+/**
+ * main - Work out the first ten digits of the sum of
+ *  one-hundred 50-digit numbers.
+ * Return: 0 if success, 1 if fail
+ */
+int main(void)
+{
+	;
+}
