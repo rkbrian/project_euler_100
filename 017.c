56c331e29e4e6a5cf658a0031f694ec695f5bb67
Ran Kong <rkbriankong@gmail.com>
2022-02-23 21:15:45 -0600

    017

--- 017.c
+++ 017.c
@@ -14,12 +14,24 @@
  */
 int main(void)
 {
-	int sings[] = {3, 3, 5, 4, 4, 3, 5, 5, 4, 3};/*1-10*/
-	int teens[] = {6, 6, 8, 8, 7, 7, 9, 8, 8};/*11-19*/
-	int tens[] = {6, 6, 5, 5, 5, 7, 6, 6};/*20, 30, 40... 90*/
+	int sings[] = {3, 3, 5, 4, 4, 3, 5, 5, 4};/*1-9*/
+	int teens[] = {3, 6, 6, 8, 8, 7, 7, 9, 8, 8};/*10-19*/
+	int tens[] = {0, 0, 6, 6, 5, 5, 5, 7, 6, 6};/*20, 30, 40... 90*/
 	int hunds[] = {7, 10};/*-hundred, -hundred and-*/
-	int thous[] = 11;/*finish line*/
-	int i = 0, sum = 0;
+	int thous = 11;/*finish line*/
+	int i = 0, sum = 0, tmp = 0;
 
-	/**/
+	for (i = 0; i < 9; i++) /*1-9*/
+		tmp += sings[i];
+	sum += tmp * 9; /*singles in 100*/
+	for (i = 0; i < 10; i++) /*complete 20-99*/
+		sum += 10 * tens[i];
+	for (i = 0; i < 10; i++) /*oddballs*/
+		sum += teens[i];
+	printf("Checking 1-99: %d, good\n", sum);
+	sum = sum * 10; /*2 digits in 1000, 000's to 900's*/
+	sum += tmp * 100 + hunds[0] * 9 + hunds[1] * 99 * 9; /*1-999*/
+	sum += thous;
+	printf("Final ans: %d\n", sum);
+	return (0);
 }
