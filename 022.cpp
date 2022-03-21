48fc73d4c114079676a6aef922cd50dfc19f8643
Ran Kong <rkbriankong@gmail.com>
2022-03-21 15:33:18 -0500

    022 progress 2

--- 022.cpp
+++ 022.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <string>
-#include <fstream>
+#include <limits.h>
+#include <unistd.h>
 #include <atomic>
 
 using namespace std;
 
 class Node {
-        public:
-        string st;
-        Node *next;
+	public:
+		string st;
+		Node *next;
 };
+
+Node *createnode(char *st)
+{
+	Node *nodey;
+
+	nodey = new Node();
+	if (!nodey || !st)
+		return NULL;
+	nodey->st = strdup(st), nodey->next = NULL;
+        return nodey;
+}
+
+void node_connect(Node *head, Node *newnode)
+{
+	if (!newnode)
+		return;
+	if (!)
+}
 
 /**
  * main - Using names.txt, a 46K text file containing over 5000 first names,
@@ -24,9 +46,22 @@ class Node {
  */
 int main()
 {
-        ifstream infile;
-        Node *heads
+	Node *heads[26], *tmp;
+	FILE *fp = fopen("p022_names.txt", "rb");
+	char *lines = NULL, *token = NULL, *lsave = NULL;
+	char *totoken = NULL, *sublsave = NULL;
+	ssize_t reading;
+	size_t len = 0;
+	long i, j, size = 0, **arr, sum = 0, triangle_size = 15;
 
-        infile.open("p022_names.txt");
-        return 0;
+	if (fp == NULL)
+		exit(EXIT_FAILURE);
+	while ((reading = getline(&lines, &len, fp)) != -1)
+	{
+		token = strtok_r(lines, ",", &lsave);
+		if (token)
+			tmp = createnode(token);
+	}
+	
+	return 0;
 }
