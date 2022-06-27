1b2047bff780f7ed29eb81f69a2e1e0a257e531d
Ran Kong <rkbriankong@gmail.com>
2022-06-26 20:44:01 -0500

    032

--- 032.cpp
+++ 032.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <math.h>
 #include <atomic>
 
 using namespace std;
+
+/**
+ * unique_int - function to define if all digits are unique in a number
+ * @num: number to be checked
+ * Return: 0 if all digits are unique, 1 otherwise
+ */
+int unique_int(int num)
+{
+        vector<int> hashTable = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int i = 0, digit = 0;
+
+        while (num)
+                digit = num % 10, hashTable[digit]++, num = num / 10;
+        for (i = 0; i < 10; i++)
+        {
+                if (hashTable[i] > 1) // each digit must be unique or not exist in the number
+                        return 1;
+        }
+        return 0;
+}
+
+/**
+ * pandigital_test - function to find if all digits from the given numbers
+ *  are pandigital from 1 to 9
+ * @a: the 1st number to be checked
+ * @b: the 2nd number to be checked
+ * @ab: the 3rd number to be checked
+ * Return: 0 if all digits are pandigital, 1 otherwise
+ */
+int pandigital_test(int a, int b, int ab)
+{
+        vector<int> hashTable = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int i = 0, digit = 0;
+
+        while (a)
+                digit = a % 10, hashTable[digit]++, a = a / 10;
+        while (b)
+                digit = b % 10, hashTable[digit]++, b = b / 10;
+        while (ab)
+                digit = ab % 10, hashTable[digit]++, ab = ab / 10;
+        for (i = 0; i < 10; i++)
+        {
+                if (hashTable[i] != 1) // each digit must be unique and exist in the number
+                        return 1;
+        }
+        return 0;
+}
 
 /**
  * main - We shall say that an n-digit number is pandigital if it makes use
@@ -15,23 +64,40 @@ using namespace std;
  *  Hint: Some products can be obtained in more than one way so be sure to only
  *  include it once in your sum.
  * Return: 0 if success, 1 if fail
+ * Note: nested loops of 3, but I managed to reduce the ranges of the
+ *  multiplicand and the multiplier, the 3rd loop max length is 9.
  */
 int main()
 {
-        // UNFINISHED
-        vector<char> a = {}, b = {}, ab = {}, ans = {};
-        vector<char> source = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        int i;
+        int a = 2, b, amax, bmax, ab = 0, sum = 0;
+        set<int> products; // set records unique values only
 
-        // a cannot be a factor because it makes b = ab, starting from 2
+        // a cannot be 1 because it makes b = ab, so starting from 2
         // let a be the smaller factor, b be the bigger one
-        while ()
+        // min a = 2, max b share equal amount of digits with max ab, max b length = 4
+        bmax = 9876 / 2;
+        while (unique_int(bmax))
+                bmax--;
+        amax = sqrt(bmax);
+        while (a < amax)
         {
-                b;
-                while ()
+                while (unique_int(a))
+                        a++;
+                b = bmax;
+                while (b > amax)
                 {
-                        ;
+                        ab = a * b;
+                        if (pandigital_test(a, b, ab) == 0)
+                        {
+                                cout << "a: " << a << "; b: " << b << "; ab: " << ab << endl;
+                                products.insert(ab);
+                        }
+                        b--;
                 }
+                a++;
         }
+        for (auto i = products.begin(); i != products.end(); i++)
+                cout << *i << " ", sum += *i;
+        cout << endl << "final ans: " << sum << endl;
         return 0;
 }
