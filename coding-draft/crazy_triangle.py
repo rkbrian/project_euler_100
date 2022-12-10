fe38a29672bec01984ef253d1965d8c76b63b9d6
Ran Kong <rkbriankong@gmail.com>
2022-12-10 14:46:22 -0600

    coding challenge question

---
+++ coding-draft/crazy_triangle.py
@@ -0,0 +1,60 @@
+def triangle(row):
+        if row is None or len(row) == 0:
+                return ""
+        elif len(row) == 1:
+                return row[0]
+        stry = "RGB"
+        dicty = {'R':0, 'G':1, 'B':2}
+        temp = []
+        rowCopy = []
+        for i in range(len(row)):
+                rowCopy.append(dicty[row[i]])
+        while len(rowCopy) > 1:
+                for i in range(len(rowCopy) - 1):
+                        temp.append((6 - rowCopy[i] - rowCopy[i + 1]) % 3)
+                rowCopy = temp
+                temp = []
+        return stry[rowCopy[0]]
+
+def superTriangle(row):
+        if row is None or len(row) == 0:
+                return ""
+        elif len(row) == 1:
+               return row[0]
+        stry = "RGB"
+        dicty = {'R':0, 'G':1, 'B':2}
+        goal = 0
+        pascal = []
+        k = 1
+        size = int(len(row))
+        switcher = -1
+        for i in range(size):
+                pascal.append(k)
+                k = k * (size - 1 - i)
+                k = k // (i + 1)
+        if size % 2 == 0:
+                switcher = -1
+        else:
+                switcher = 1
+        for i in range(size):
+                goal += switcher * pascal[i] * dicty[row[i]]
+        return stry[int(goal) % 3]
+
+if __name__ == "__main__":
+        basic_cases = [
+    ['B', 'B'],
+    ['GB', 'R'],
+    ['RRR', 'R'],
+    ['RGBG', 'B'],
+    ['RBRGBRB', 'G'],
+    ['RBRGBRBGGRRRBGBBBGG', 'G']
+]
+        longStr = 'RBRGBRBGGRRRBGBBBGG' * 5000
+        longStrt = 'G' * 50000
+        for i in basic_cases:
+                print("Regular logic:      " + triangle(i[0]))
+                print("Pascal coefficient: " + superTriangle(i[0]))
+                print("Real value:         " + i[1])
+        print('================')
+        print("Super long: " + superTriangle(longStr))
+        print("Super long: " + superTriangle(longStrt))
