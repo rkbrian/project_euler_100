b7ca8965d05759c0ba64ec4cd79c9da05035f7dc
Ran Kong <rkbriankong@gmail.com>
2023-01-22 03:40:51 -0600

    expert level revision and documentary

--- sudoku_generator/main.py
+++ sudoku_generator/main.py
@@ -1,18 +1,34 @@
 """
 -- Prologue --
 My mother is a Sudoku zealot, she buys tons of Sudoku puzzle books to play.
-As a birthday gift to her, I made this program that can generate Sudoku puzzles.
-(And to save her money on buying those books! 😡)
-This is a hack project based on the following link:
+I made this program that can generate Sudoku puzzles, which I plan to give to her
+as a birthday gift. (And this should save her money from buying more books! 😡)
+I didn't invent the wheel, it is a hack project based on the following link:
     Sudoku Generator Algorithm - www.101computing.net/sudoku-generator-algorithm/
 
--- Progress --
-The original code on this site generates a random Sudoku puzzle, but I want more!
-I want to control it with inputs for the following parameters:
-    1. Diffculty (Easy-Medium-Hard-Expert, done)
-    2. Layout preference (No_preference-Symmetrical-Axial, done)
-    3. Expert level: No hint of a certain number (e.g. all given numbers in the grid
-        does not include "1", not started yet)
+-- Develop Progress --
+The original code was very basic, it generates a random Sudoku puzzle, but I wanted more!
+I wanted to add controls with inputs for the following parameters:
+    1. Diffculty (Easy / Medium / Hard / Expert) ---- done
+    2. Layout preference (No preference / Symmetrical / Axial) ---- done
+    3. Expert level: one certain number is not hinted (e.g. all given numbers in the grid
+        does not include "7") ---- done
+
+-- Features --
+1. Selectable difficulty (4 choices)
+2. Selectable preference of the given number layout
+    (no preference / random, symmetrical layout, axial layout)
+    (for expert level difficulty, only random layout is available)
+3. Expert level difficulty: the clue numbers are not all 9 types, it is missing one type
+4. Generating a solution / answer grid is optional, before generating the puzzle
+    (answer would be shown for 10 seconds, then turn into puzzle grid)
+5. Python-Turtle window drawing the grid and the numbers
+
+-- Outcome --
+Total hours: 8
+I'm very satisfied! Score 10/10!
+Issues: needs a method to download the puzzle or the answer grid, right now the user
+    has to take screenshots.
 """
 import turtle
 import datetime
@@ -22,7 +38,7 @@ from time import sleep
 
 class Difficulty(Enum):
     EASY = 1
-    MIDDLE = 2
+    MEDIUM = 2
     HARD = 3
     EXPERT = 4
 
@@ -47,17 +63,21 @@ while (exitCode):
     diffNum = input('Select diffculty: 1-Easy, 2-Medium, 3-Hard, 4-Expert\n')
     if (inputValidator(diffNum) in (item.value for item in Difficulty)):
         while (exitCode):
-            preferenceNum = input('Select layout preference: 1-no preference, 2-symmetrical, 3-axial\n')
+            if (int(diffNum) == Difficulty.EXPERT.value):
+                print('*Expert level is only available in random layout.')
+                preferenceNum = '1'
+            else:
+                preferenceNum = input('Select layout preference: 1-no preference, 2-symmetrical, 3-axial\n')
             if (inputValidator(preferenceNum) in (item.value for item in Preference)):
                 while (exitCode):
-                    answerBool = input('Do you want to save an answer grid? (Y/N)\n')
+                    answerBool = input('Do you want to have an answer grid? (Y/N)\n')
                     if (answerBool.lower() == 'y' or answerBool.lower() == 'yes'):
                         sleepSeconds = 10
-                        attempts = int(diffNum) * 5
+                        attempts = 2 + int(diffNum) * 3
                         exitCode = 0
                         print('You have {} seconds to take the screenshot.'.format(sleepSeconds))
                     elif (answerBool.lower() == 'n' or answerBool.lower() == 'no'):
-                        attempts = int(diffNum) * 5
+                        attempts = 2 + int(diffNum) * 3
                         exitCode = 0
                     else:
                         print('Please answer yes or no.')
@@ -106,7 +126,7 @@ def drawGrid(grid, visibility = True):
     for row in range (0, 9):
         for col in range (0, 9):
             if grid[row][col] != 0:
-                text(grid[row][col], topLeft_x + col * intDim + 9, topLeft_y - row * intDim - intDim + 8, 18, visibility)
+                text(grid[row][col], topLeft_x + col * intDim + 13, topLeft_y - row * intDim - intDim + 3, 18, visibility)
 
 """A function to check if the grid is full"""
 def checkGrid(grid):
@@ -119,20 +139,20 @@ def checkGrid(grid):
 """A backtracking/recursive function to check all possible combinations of numbers until a solution is found"""
 def solveGrid(grid):
     global counter
-    #Find next empty cell
+    # Find next empty cell
     for i in range(0, 81):
         row = i // 9
         col = i % 9
         if grid[row][col] == 0:
             for value in range (1, 10):
-                #Check that this value has not already be used on this row and this column
+                # Check that this value has not already be used on this row and this column
                 cols = (grid[x][col] for x in range(9))
                 if not(value in grid[row]) and not(value in cols):
-                    #Identify which of the 9 squares we are working on
+                    # Identify which of the 9 squares we are working on
                     a = row // 3
                     b = col // 3
                     square = [grid[i][b * 3 : (b + 1) * 3] for i in range(a * 3, (a + 1) * 3)]
-                    #Check that this value has not already be used on this 3x3 square
+                    # Check that this value has not already be used on this 3x3 square
                     if not value in (square[0] + square[1] + square[2]):
                         grid[row][col] = value
                         if checkGrid(grid):
@@ -149,21 +169,21 @@ numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 """A backtracking/recursive function to check all possible combinations of numbers until a solution is found"""
 def fillGrid(grid):
     global counter
-    #Find next empty cell
+    # Find next empty cell
     for i in range(0,81):
         row = i // 9
         col = i % 9
         if grid[row][col] == 0:
             shuffle(numberList)
             for value in numberList:
-                #Check that this value has not already be used on this row and this column
+                # Check that this value has not already be used on this row and this column
                 cols = (grid[x][col] for x in range(9))
                 if not(value in grid[row]) and not(value in cols):
-                        #Identify which of the 9 squares we are working on
+                        # Identify which of the 9 squares we are working on
                         a = row // 3
                         b = col // 3
                         square = [grid[i][b * 3 : (b + 1) * 3] for i in range(a * 3, (a + 1) * 3)]
-                        #Check that this value has not already be used on this 3x3 square
+                        # Check that this value has not already be used on this 3x3 square
                         if not value in (square[0] + square[1] + square[2]):
                             grid[row][col] = value
                             if checkGrid(grid):
@@ -189,14 +209,21 @@ Potentially resulting in more difficiult grids to solve!
 """
 counter = 1
 prefNum = int(preferenceNum)
+# Difficulty: EXPERT has 8 types of numbers at most
+if (int(diffNum) == Difficulty.EXPERT.value):
+    extinctionNum = randint(1, 9)
+    for i in range(9):
+        for j in range(9):
+            if (grid[i][j] == extinctionNum):
+                grid[i][j] = 0
 while attempts > 0:
-    #Select a random cell that is not already empty
+    # Select a random cell that is not already empty
     row = randint(0, 8)
     col = randint(0, 8)
     while grid[row][col] == 0:
         row = randint(0, 8)
         col = randint(0, 8)
-    #Remember its cell value in case we need to put it back
+    # Remember its cell value in case we need to put it back
     backup = grid[row][col]
     grid[row][col] = 0
     if ((prefNum == Preference.SYMMETRICAL.value and col != 4) or (prefNum == Preference.AXIAL.value and not(row == 4 and col == 4))):
@@ -206,21 +233,21 @@ while attempts > 0:
         twincol = 8 - col
         twinbackup = grid[twinrow][twincol]
         grid[twinrow][twincol] = 0
-    #Take a full copy of the grid
+    # Take a full copy of the grid
     copyGrid = []
     for r in range(0, 9):
         copyGrid.append([])
         for c in range(0, 9):
             copyGrid[r].append(grid[r][c])
-    #Count the number of solutions that this grid has (using a backtracking approach implemented in the solveGrid() function)
+    # Count the number of solutions that this grid has (using a backtracking approach implemented in the solveGrid() function)
     counter = 0
     solveGrid(copyGrid)   
-    #If the number of solution is different from 1 then we need to cancel the change by putting the value we took away back in the grid
+    # If the number of solution is different from 1 then we need to cancel the change by putting the value we took away back in the grid
     if counter != 1:
         grid[row][col] = backup
         if ((prefNum == Preference.SYMMETRICAL.value and row != 4) or (prefNum == Preference.AXIAL.value and not(row == 4 and col == 4))):
             grid[twinrow][twincol] = twinbackup
-        #We could stop here, but we can also have another attempt with a different cell just to try to remove more numbers
+        # We could stop here, but we can also have another attempt with a different cell just to try to remove more numbers
         attempts -= 1
     myPen.clear()
     drawGrid(grid, False)
@@ -228,7 +255,7 @@ while attempts > 0:
 myPen.clear()
 drawGrid(grid)
 myPen.getscreen().update()
-codeMessage = str(datetime.date.today()) + '     created by: Brian Kong'
+codeMessage = 'Difficulty: ' + Difficulty(int(diffNum)).name.lower() + '\nCreated by: Brian Kong\n' + str(datetime.date.today())
 text(codeMessage, topLeft_x + 0 * 35 + 9, topLeft_y - 10 * 35 - 35 + 8, 10)
 print('Sudoku is successfully generated!')
 turtle.mainloop()
